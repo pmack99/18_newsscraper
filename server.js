c617b0d303e81de163558d076d6e5768a8f2f034
PMack <prestonmack@gmail.com>
2018-12-18 21:10:19 -0500

    initial push

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var port = process.env.PORT || 3000;
+var app = express();
+
+// Use body parser with the app
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
